d785a648a36ff2fc5154b2576c33a44148e07485
pavelminsky <pavelminsky@gmail.com>
2013-02-28 16:58:31 +0400

    test upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>Последнее изгнание дьявола: Второе пришествие — В кино с 1 марта</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Последнее изгнание дьявола: Второе пришествие. Официальный сайт фильма. В кино с 1 марта.">
+<meta name="keywords" content="Последнее изгнание дьявола, Второе пришествие, The Last Exorcism Part II, ужасы, триллер, фильм, Эд Гасс-Доннели, Спенсер Трит Кларк, Эшли Белл, Эндрю Сенсениг, Джудд Лорманд, Джулия Гарнер">
+
+<meta property="og:title" content="Последнее изгнание дьявола: Второе пришествие - официальный сайт фильма">
+<meta property="og:description" content="Нелл, героиня первой части фильма, пытается начать новую жизнь. Однако старое зло возвращается. С ещё более ужасным планом.">
+<meta property="og:type" content="movie">
+<meta property="og:url" content="http://www.lastexorcism2.ru">
+<meta property="og:image" content="http://www.lastexorcism2.ru/images/share.jpg">
+<meta property="og:site_name" content="Прекрасные создания - официальный сайт фильма">
+
+<meta name="viewport" content="width=990">
+<meta name="format-detection" content="telephone=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="apple-mobile-web-app-title" content="Изыди!">
+<meta name="google-site-verification" content="UZ0PMPnmp6bpeBzySISW3jqhu8neOj2y2cwi4xaAYWc" />
+<meta name='wmail-verification' content='d55a255c04d0520d' />
+<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="apple-touch-icon-precomposed" href="/apple-touch-icon.png">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="favicon.png">
+
+<meta itemprop="name" content="Последнее изгнание дьявола: Второе пришествие - официальный сайт фильма">
+<meta itemprop="description" content="Нелл, героиня первой части фильма, пытается начать новую жизнь. Однако старое зло возвращается. С ещё более ужасным планом.">
+<meta itemprop="image" content="http://www.lastexorcism2.ru/images/share.jpg">
+
+<link rel="stylesheet" href="/css/main.css">
+<link rel="stylesheet" type="text/css" href="/css/fancybox.css?v=2.1.1" media="screen">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Playfair+Display+SC:400,400italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+
+<link href="/css/video-js.css" rel="stylesheet">
+<script type="text/javascript" src="/js/video.js"></script>
+<style type="text/css">
+.vjs-default-skin .vjs-play-progress {background: #969696;}
+.vjs-default-skin .vjs-load-progress {background: #5a5a5a;}
+.vjs-default-skin .vjs-seek-handle {display: none;}
+.vjs-default-skin .vjs-current-time-display, .vjs-default-skin .vjs-remaining-time-display {font: normal 10px Arial; color: #6f7374; text-shadow: none;}
+</style>
+
+</head><body>
+
+<div class="age">&nbsp;</div>
+<div class="puzzle"><a href="/puzzle/">&nbsp;</a></div>
+
+<div class="wrap">
+
+	<div class="top">&nbsp;</div>
+
+	<div id="lastexorcism2_russiantrailer_550" style="width: 800px; height: 332px; margin: 0 auto;"><video class="video-js vjs-default-skin" controls preload="auto" width="800" height="332" poster="/video/lastexorcism2_russiantrailer_550.jpg" data-setup="{}"><source src="http://www.lastexorcism2.ru/video/lastexorcism2_russiantrailer_550.mp4" type='video/mp4'><source src="http://www.lastexorcism2.ru/video/lastexorcism2_russiantrailer_550.ogv" type='video/ogg'></video></div>
+
+	<div class="content clearfix">
+		<div class="left">&nbsp;</div>
+		<div class="right">
+			<p><b>Нелл находят в лесу</b> — грязную и напуганную, только что сбежавшую со страшного ритуала. Вся её семья погибла, и девушку отправляют в Новый Орлеан — приют для девочек. Там она попадает под присмотр терапевта Фрэнка Мерле. Жизнь вроде бы налаживается, Нелл влюбляется в местного мальчика, но в самый неподходящий момент демон вновь объявляется и вселяется в Нелл. Теперь он хочет от неё чего-то большего...</p>
+			<p style="color: #9d1e00;">Первый фильм вышел в прокат в 2010 году.</p>
+			<p style="color: #9d1e00;"><b>Второе пришествие Зла на российские экраны состоится 1 марта 2013 года.</b></p>
+			<p>&nbsp;</p>
+			<p><b>Режиссёр:</b> Эд Гасс-Доннели</p>
+			<p><b>Сценарий:</b> Дэмьен Шазелл, Эд Гасс-Доннели</p>
+			<p><b>В ролях:</b> Спенсер Трит Кларк, Эшли Белл, Эндрю Сенсениг, Джудд Лорманд, Джулия Гарнер, Мьюз Уотсон, Э. Роджер Митчелл</p>
+		</div>
+	</div>
+
+	<p><a class="stills" data-fancybox-group="gallery_stills" href="/media/01.jpg"><img src="/media/01s.png" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/02.jpg"><img src="/media/02s.png" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/03.jpg"><img src="/media/03s.png" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/04.jpg"><img src="/media/04s.png" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/05.jpg"><img src="/media/05s.png" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/06.jpg"><img src="/media/06s.png" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/07.jpg"><img src="/media/07s.png" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/08.jpg"><img src="/media/08s.png" alt=""></a></p>
+
+	<div class="copy">&copy; 2013 &laquo;<a href="http://www.westvideo.ru">Вест Видео</a>&raquo;. Создание сайта: <a href="http://www.kg-design.ru/">КГ-Дизайн</a></div>
+
+	<div id="counters">
+		<div class="counters_list">
+		<p>Сюда изгнаны счетчики:</p>
+		<p>
+		<?php include ("counters_include.php"); ?>	
+		</p>
+		</div>
+	</div>
+
+</div>
+
+<!-- Fancybox -->
+<script type="text/javascript" src="/js/jquery.fancybox.js?v=2.1.1"></script>
+<script type="text/javascript" src="/js/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+<script type="text/javascript" src="/js/jquery.fancybox-media.js?v=1.0.4"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+$('.stills').fancybox({
+//prevEffect : 'none',
+//nextEffect : 'none',
+//closeBtn  : false,
+//arrows    : false,
+nextClick : false,
+'margin' : 50,
+helpers : {thumbs : {width  : 50, height : 50}}
+});
+$('.videos').fancybox({
+//prevEffect : 'none',
+//nextEffect : 'none',
+//closeBtn  : false,
+arrows    : false,
+nextClick : false,
+helpers : {thumbs : {width  : 90, height : 50}}
+});
+});
+</script>
+
+</body></html>
