60662f4e924d78fdde3f51bd9332a27342622c39
pavelminsky <pavelminsky@gmail.com>
2013-03-01 19:37:44 +0400

    beta - deployed

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Гамбит. Официальный сайт фильма. В кино с 7 марта.">
-<meta name="keywords" content="Гамбит, Gambit, криминальная комедия, фильм, Майкл Хоффман, Камерон Диаз, Колин Ферт, Алан Рикман, Стэнли Туччи, братья Коэны, Джоэл Коэн, Итан Коэн">
+<meta name="keywords" content="Гамбит, Gambit, криминальная комедия, фильм, Майкл Хоффман, Кэмерон Диаз, Колин Ферт, Алан Рикман, Стэнли Туччи, братья Коэны, Джоэл Коэн, Итан Коэн">
 
 <meta property="og:title" content="Гамбит - официальный сайт фильма">
 <meta property="og:description" content="Искусствовед-авантюрист, звезда родео, злобный миллиардер и поддельное полотно Моне в римейке одноименной криминальной комедии!">
 <meta property="og:type" content="movie">
-<meta property="og:url" content="http://www.url.ru">
-<meta property="og:image" content="http://www.url.ru/images/share.jpg">
+<meta property="og:url" content="http://www.gambitmovie.ru">
+<meta property="og:image" content="http://www.gambitmovie.ru/images/share.jpg">
 <meta property="og:site_name" content="Гамбит - официальный сайт фильма">
 
 <meta name="viewport" content="width=990">
@@ -26,12 +26,18 @@
 
 <meta itemprop="name" content="Гамбит - официальный сайт фильма">
 <meta itemprop="description" content="Искусствовед-авантюрист, звезда родео, злобный миллиардер и поддельное полотно Моне в римейке одноименной криминальной комедии!">
-<meta itemprop="image" content="http://www.url.ru/images/share.jpg">
+<meta itemprop="image" content="http://www.gambitmovie.ru/images/share.jpg">
 
-<link rel="stylesheet" href="/css/main.css">
+<link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" type="text/css" href="/css/fancybox.css?v=2.1.1" media="screen">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="/js/init.js"></script>
+<script>
+jQuery(function($) {
+$('.dim').fadeOut(500);
+});
+</script>
 
 <link href="/css/video-js.css" rel="stylesheet">
 <script type="text/javascript" src="/js/video.js"></script>
@@ -44,43 +50,56 @@
 
 </head><body>
 
-<div class="age noSelect">&nbsp;</div>
-<div class="testcornertl noSelect">&nbsp;</div>
-<div class="testcornertr noSelect">&nbsp;</div>
-<div class="testcornerdl noSelect">&nbsp;</div>
-<div class="testcornerdr noSelect">&nbsp;</div>
+<noscript><style>.dim {display: none;}</style></noscript>
+<div class="dim">&nbsp;</div>
+
+<div class="header">
+	<div class="cul noselect">&nbsp;</div>
+	<div class="cover">&nbsp;</div>
+	<div class="cur noselect">&nbsp;</div>
+</div>
 
 
-<div class="wrap">
 
-	<div class="top">&nbsp;</div>
 
-	<div id="lastexorcism2_russiantrailer_550" style="width: 800px; height: 332px; margin: 0 auto;"><video class="video-js vjs-default-skin" controls preload="auto" width="800" height="332" poster="/video/lastexorcism2_russiantrailer_550.jpg" data-setup="{}"><source src="http://www.lastexorcism2.ru/video/lastexorcism2_russiantrailer_550.mp4" type='video/mp4'><source src="http://www.lastexorcism2.ru/video/lastexorcism2_russiantrailer_550.ogv" type='video/ogg'></video></div>
 
-	<div class="content clearfix">
-		<div class="left">&nbsp;</div>
-		<div class="right">
-			<p>12+</p>
-			<p>режиссер Майкл Хоффман</p><p>сценарий Джоэл Коэн, Итан Коэн</p><p>криминальная комедия</p><p>В главных ролях: Кэмерон Диаз, Алан Рикман, Колин Фёрт, Стэнли Туччи, Клорис Личмен, Анна Скеллерн, Том Кортни, Того Игава, Эрика ЛаРоуз, Танрох Ишида</p>
-			<p><b>"Гамбит"</b> — римейк одноименной криминальной комедии 1966 года.</p>
-			<p>Главный герой фильма — лондонский искусствовед Гарри Дин (<b>Колин Фёрт</b>). Авантюрист задумал обмануть богатого коллекционера Лайонела Шабандара (<b>Алан Рикман</b>), продав ему фальшивую картину Моне.</p>
-			<p>Для этого Гарри вступает в сговор с эксцентричной и разбитной девицей из Техаса по имени Пи-Джей Пузновски (<b>Камерон Диаз</b>). Пи-Джей не имеет никакого отношения к живописи — она работает на родео. Тем не менее, ей придется выдавать себя за женщину, чей дед спас картину из нацистской Германии в конце Второй мировой войны.</p>
-			<p>&nbsp;</p>
-		</div>
+
+<!-- Говнокод -->
+<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+
+<div id="lastexorcism2_russiantrailer_550" style="width: 800px; height: 335px; margin: 0 auto;"><video class="video-js vjs-default-skin" controls preload="auto" width="800" height="335" poster="/video/gambit_russiantrailer_550.jpg" data-setup="{}"><source src="http://www.gambitmovie.ru/video/gambit_russiantrailer_550.mp4" type='video/mp4'><source src="http://www.gambitmovie.ru/video/gambit_russiantrailer_550.ogv" type='video/ogg'></video></div>
+
+<div class="tempwrap clearfix" style="width: 780px; margin: 30px auto 40px auto; text-align: center;">
+
+	<div class="temptrailer" style="float: left; width: 200px; height: 200px; background: url('/images/age.png') center center no-repeat;">&nbsp;</div>
+
+	<div class="tempinfo" style="float: right; width: 580px; margin: 20px 0 20px 0; font-size: 1em; line-height: 1.5em; text-align: left; text-shadow: 0 1px 1px #ffffff;">
+	<p style="padding-bottom: 8px; text-indent: -0.6em;"><b>&laquo;Гамбит&raquo;</b> — римейк одноименной криминальной комедии 1966 года.</p>
+	<p style="padding-bottom: 8px;">Главный герой фильма — лондонский искусствовед Гарри Дин (<b>Колин Фёрт</b>). Авантюрист задумал обмануть богатого коллекционера Лайонела Шабандара (<b>Алан Рикман</b>), продав ему фальшивую картину Моне.</p>
+	<p>Для этого Гарри вступает в сговор с эксцентричной и разбитной девицей из Техаса по имени Пи-Джей Пузновски (<b>Кэмерон Диаз</b>). Пи-Джей не имеет никакого отношения к живописи — она выступает на родео. Тем не менее, ей придется выдавать себя за женщину, чей дед спас полотно Моне из нацистской Германии в конце Второй мировой войны.</p>
+	<p>&nbsp;</p>
+	<p style="font-size: 0.8em; padding-bottom: 6px; line-height: 1.2em;"><b>Режиссер:</b> Майкл Хоффман</p>
+	<p style="font-size: 0.8em; padding-bottom: 6px; line-height: 1.2em;"><b>Сценаристы:</b> Джоэл Коэн и Итан Коэн</b>
+	<p style="font-size: 0.8em; padding-bottom: 6px; line-height: 1.2em;"><b>В ролях:</b> В главных ролях: Кэмерон Диаз, Алан Рикман, Колин Фёрт, Стэнли Туччи, Клорис Личмен, Анна Скеллерн, Том Кортни, Того Игава, Эрика ЛаРоуз, Танрох Ишида</p>
 	</div>
 
-	<p><a class="stills" data-fancybox-group="gallery_stills" href="/media/01.jpg"><img src="/media/01s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/02.jpg"><img src="/media/02s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/03.jpg"><img src="/media/03s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/04.jpg"><img src="/media/04s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/05.jpg"><img src="/media/05s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/06.jpg"><img src="/media/06s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/07.jpg"><img src="/media/07s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/08.jpg"><img src="/media/08s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/09.jpg"><img src="/media/09s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/10.jpg"><img src="/media/10s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/11.jpg"><img src="/media/11s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/12.jpg"><img src="/media/12s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/13.jpg"><img src="/media/13s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/14.jpg"><img src="/media/14s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/15.jpg"><img src="/media/15s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/16.jpg"><img src="/media/16s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/17.jpg"><img src="/media/17s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/18.jpg"><img src="/media/18s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/19.jpg"><img src="/media/19s.jpg" alt=""></a></p>
+</div>
+<!-- //Говнокод -->
+
+
+
+
+
+<div id="counters"><div class="counters_list"><?php include ("counters_include.php"); ?></div></div>
+
+<div class="footer">
+	<div class="cdl noselect">&nbsp;</div>
+
+	<div class="gallery"><p><a class="stills" data-fancybox-group="gallery_stills" href="/media/01.jpg"><img src="/media/01s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/02.jpg"><img src="/media/02s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/03.jpg"><img src="/media/03s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/04.jpg"><img src="/media/04s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/05.jpg"><img src="/media/05s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/06.jpg"><img src="/media/06s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/07.jpg"><img src="/media/07s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/08.jpg"><img src="/media/08s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/09.jpg"><img src="/media/09s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/10.jpg"><img src="/media/10s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/11.jpg"><img src="/media/11s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/12.jpg"><img src="/media/12s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/13.jpg"><img src="/media/13s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/14.jpg"><img src="/media/14s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/15.jpg"><img src="/media/15s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/16.jpg"><img src="/media/16s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/17.jpg"><img src="/media/17s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/18.jpg"><img src="/media/18s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/19.jpg"><img src="/media/19s.jpg" alt=""></a></p></div>
 
 	<div class="copy">&copy; 2013 &laquo;<a href="http://www.westvideo.ru">Вест Видео</a>&raquo;. Создание сайта: <a href="http://www.kg-design.ru/">КГ-Дизайн</a></div>
 
-	<div id="counters">
-		<div class="counters_list">
-		<p>
-		<?php include ("counters_include.php"); ?>	
-		</p>
-		</div>
-	</div>
-
+	<div class="cdr noselect">&nbsp;</div>
 </div>
 
 <!-- Fancybox -->
